d766a6a537202a197983963ed255e317d7ba10ab
jangjisu <jsjangdv@gmail.com>
2025-04-23 20:14:55 +0900

    initialize Spring Boot project with Gradle setup and basic application structure

---
+++ src/main/java/com/game/janggi/JanggiApplication.java
@@ -0,0 +1,13 @@
+package com.game.janggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanggiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JanggiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/game/janggi/JanggiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.janggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanggiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
