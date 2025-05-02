fab59ebe4f4978bb5d99fb78406e1b518a98f384
jsjang <wltn85@naver.com>
2025-05-02 16:19:32 +0900

    포메이션타입에 넘겨받을 숫자 추가

--- src/main/java/com/game/janggi/domain/formation/FormationType.java
+++ src/main/java/com/game/janggi/domain/formation/FormationType.java
@@ -4,10 +4,21 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum FormationType {
-    SANG_MA_MA_SANG("상마마상"),
-    SANG_MA_SANG_MA("상마상마"),
-    MA_SANG_MA_SANG("마상마상"),
-    MA_SANG_SANG_MA("마상상마");
+    SANG_MA_MA_SANG("상마마상", 1),
+    SANG_MA_SANG_MA("상마상마", 2),
+    MA_SANG_MA_SANG("마상마상", 3),
+    MA_SANG_SANG_MA("마상상마", 4);
 
     private final String description;
+    private final int inputNumber;
+
+    public static FormationType getFromInputNumber(int inputNumber) {
+        for (FormationType formationType : FormationType.values()) {
+            if (formationType.inputNumber == inputNumber) {
+                return formationType;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid input number: " + inputNumber);
+    }
 }
