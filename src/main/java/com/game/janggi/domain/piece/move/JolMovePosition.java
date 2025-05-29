e7782b7bd5598085fbc49644448d9d545e01652d
jangjisu <jsjangdv@gmail.com>
2025-05-29 21:12:32 +0900

    병, 졸도 궁 안에서는 대각선 이동 가능하도록 수정

--- src/main/java/com/game/janggi/domain/piece/move/JolMovePosition.java
+++ src/main/java/com/game/janggi/domain/piece/move/JolMovePosition.java
@@ -1,6 +1,7 @@
 package com.game.janggi.domain.piece.move;
 
 import com.game.janggi.domain.piece.Piece;
+import com.game.janggi.domain.piece.position.GongPiecePosition;
 import com.game.janggi.domain.piece.position.PiecePosition;
 import com.game.janggi.domain.team.TeamType;
 
@@ -12,6 +13,14 @@ public class JolMovePosition extends MovePosition {
             Directions.create(Direction.DOWN),
             Directions.create(Direction.LEFT),
             Directions.create(Direction.RIGHT)
+    );
+
+    private final List<Directions> diagonalMoveAbleDirections = List.of(
+            Directions.create(Direction.DOWN),
+            Directions.create(Direction.LEFT),
+            Directions.create(Direction.RIGHT),
+            Directions.create(Direction.DOWN_LEFT),
+            Directions.create(Direction.DOWN_RIGHT)
     );
 
 
@@ -26,7 +35,7 @@ public class JolMovePosition extends MovePosition {
 
     @Override
     protected List<Directions> calculateBasicMoveAbleDirections(PiecePosition currentPosition) {
-        return moveAbleDirections.stream()
+        return GongPiecePosition.canMoveDiagonal(currentPosition) ? diagonalMoveAbleDirections : moveAbleDirections.stream()
                 .filter(currentPosition::canMove)
                 .toList();
     }
