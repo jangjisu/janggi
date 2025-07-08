da33e2fd1a48d2c71c9f3146e01f2a4ed203310e
jsjang <wltn85@naver.com>
2025-07-08 10:22:23 +0900

    [test] Movements 객체 추가에 따른 테스트 변경 및 테스트 커버리지 62% -> 65% 로 증가

--- src/test/java/com/game/janggi/domain/piece/move/SaMovePositionTest.java
+++ src/test/java/com/game/janggi/domain/piece/move/SaMovePositionTest.java
@@ -1,56 +1,139 @@
 package com.game.janggi.domain.piece.move;
 
+import com.game.janggi.domain.piece.*;
 import com.game.janggi.domain.piece.position.PiecePosition;
+import com.game.janggi.domain.team.TeamType;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SaMovePositionTest {
+    Map<PiecePosition, Piece> pieces = new HashMap<>();
+
+    MovePosition movePosition = MovePosition.createSaMove();
+
+    @AfterEach
+    void tearDown() {
+        pieces.clear();
+    }
+
     @Test
-    @DisplayName("사가 궁성 중앙에 있다면 궁성 어디로든 이동할 수 있다.")
-    void moveWhenMiddle() {
+    @DisplayName("사는 궁성내부 상하좌우로 이동할 수 있다.")
+    void move() {
         //given
-        PiecePosition piecePosition = new PiecePosition(4, 1);
+        PiecePosition position = PiecePosition.create(4, 0);
+        pieces.put(position, Sa.create(TeamType.HAN));
 
         //when
-        SaMovePosition saMovePositionTest = new SaMovePosition();
-
-        Movements moveAblePosition = saMovePositionTest.calculateBasicMoveAbleDirections(piecePosition);
+        List<PiecePosition> moveAblePosition = movePosition.getMoveablePosition(pieces, position);
 
         //then
-        assertThat(moveAblePosition.getValues()).hasSize(8)
-                .contains(
-                        Movement.create(Direction.UP),
-                        Movement.create(Direction.DOWN),
-                        Movement.create(Direction.LEFT),
-                        Movement.create(Direction.RIGHT),
-                        Movement.create(Direction.UP_LEFT),
-                        Movement.create(Direction.UP_RIGHT),
-                        Movement.create(Direction.DOWN_LEFT),
-                        Movement.create(Direction.DOWN_RIGHT)
+        assertThat(moveAblePosition).hasSize(3)
+                .containsExactlyInAnyOrder(
+                        PiecePosition.create(5, 0),
+                        PiecePosition.create(3, 0),
+                        PiecePosition.create(4, 1)
+                );
+    }
+
+    @Test
+    @DisplayName("사는 궁성 중앙에 있다면 대각선으로도 이동할 수 있다.")
+    void moveDigonal() {
+        //given
+        PiecePosition position = PiecePosition.create(4, 1);
+        pieces.put(position, Sa.create(TeamType.HAN));
+
+        //when
+        List<PiecePosition> moveAblePosition = movePosition.getMoveablePosition(pieces, position);
+
+        //then
+        assertThat(moveAblePosition).hasSize(8)
+                .containsExactlyInAnyOrder(
+                        PiecePosition.create(4, 0),
+                        PiecePosition.create(4, 2),
+                        PiecePosition.create(3, 1),
+                        PiecePosition.create(5, 1),
+                        PiecePosition.create(3, 0),
+                        PiecePosition.create(3, 2),
+                        PiecePosition.create(5, 0),
+                        PiecePosition.create(5, 2)
                 );
 
     }
 
     @Test
-    @DisplayName("사가 궁성 우측에 있다면 위, 아래, 왼쪽을 움직일 수 있다.")
-    void moveWhenRightMiddle() {
+    @DisplayName("사는 움직일 위치에 상대 기물이 있더라도 이동 가능하다.")
+    void moveWhenEnemy() {
         //given
-        PiecePosition piecePosition = new PiecePosition(5, 1);
+        PiecePosition position = PiecePosition.create(4, 1);
+        pieces.put(position, Sa.create(TeamType.HAN));
+        pieces.put(PiecePosition.create(4, 0), Jol.create(TeamType.CHO));
+        pieces.put(PiecePosition.create(4, 2), Jol.create(TeamType.CHO));
+        pieces.put(PiecePosition.create(3, 1), Jol.create(TeamType.CHO));
+        pieces.put(PiecePosition.create(5, 1), Jol.create(TeamType.CHO));
+        pieces.put(PiecePosition.create(3, 0), Jol.create(TeamType.CHO));
+        pieces.put(PiecePosition.create(3, 2), Jol.create(TeamType.CHO));
+        pieces.put(PiecePosition.create(5, 0), Jol.create(TeamType.CHO));
+        pieces.put(PiecePosition.create(5, 2), Jol.create(TeamType.CHO));
 
         //when
-        SaMovePosition saMovePositionTest = new SaMovePosition();
-
-        Movements moveAblePosition = saMovePositionTest.calculateBasicMoveAbleDirections(piecePosition);
+        List<PiecePosition> moveAblePosition = movePosition.getMoveablePosition(pieces, position);
 
         //then
-        assertThat(moveAblePosition.getValues()).hasSize(3)
-                .contains(
-                        Movement.create(Direction.UP),
-                        Movement.create(Direction.DOWN),
-                        Movement.create(Direction.LEFT)
+        assertThat(moveAblePosition).hasSize(8)
+                .containsExactlyInAnyOrder(
+                        PiecePosition.create(4, 0),
+                        PiecePosition.create(4, 2),
+                        PiecePosition.create(3, 1),
+                        PiecePosition.create(5, 1),
+                        PiecePosition.create(3, 0),
+                        PiecePosition.create(3, 2),
+                        PiecePosition.create(5, 0),
+                        PiecePosition.create(5, 2)
                 );
+
+    }
+
+    @Test
+    @DisplayName("사는 움직일 위치에 같은팀 기물이 있을경우 이동 할 수 없다.")
+    void moveWhenTeam() {
+        //given
+        PiecePosition position = PiecePosition.create(4, 1);
+        pieces.put(position, Sa.create(TeamType.HAN));
+        pieces.put(PiecePosition.create(4, 0), Bung.create(TeamType.HAN));
+        pieces.put(PiecePosition.create(4, 2), Bung.create(TeamType.HAN));
+        pieces.put(PiecePosition.create(3, 1), Bung.create(TeamType.HAN));
+        pieces.put(PiecePosition.create(5, 1), Bung.create(TeamType.HAN));
+        pieces.put(PiecePosition.create(3, 0), Bung.create(TeamType.HAN));
+        pieces.put(PiecePosition.create(3, 2), Bung.create(TeamType.HAN));
+        pieces.put(PiecePosition.create(5, 0), Bung.create(TeamType.HAN));
+        pieces.put(PiecePosition.create(5, 2), Bung.create(TeamType.HAN));
+
+        //when
+        List<PiecePosition> moveAblePosition = movePosition.getMoveablePosition(pieces, position);
+
+        //then
+        assertThat(moveAblePosition).isEmpty();
+    }
+
+    @Test
+    @DisplayName("사는 궁 외부에 있을경우 이동 할 수 없다.")
+    void moveWhenNotInGong() {
+        //given
+        PiecePosition position = PiecePosition.create(7, 7);
+        pieces.put(position, Sa.create(TeamType.HAN));
+
+        //when
+        List<PiecePosition> moveAblePosition = movePosition.getMoveablePosition(pieces, position);
+
+        //then
+        assertThat(moveAblePosition).isEmpty();
 
     }
 }
--- src/test/java/com/game/janggi/domain/piece/move/SangMovePositionTest.java
+++ src/test/java/com/game/janggi/domain/piece/move/SangMovePositionTest.java
@@ -1,34 +1,12 @@
 package com.game.janggi.domain.piece.move;
 
-import com.game.janggi.domain.piece.position.PiecePosition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class SangMovePositionTest {
     @Test
     @DisplayName("상은 상,하,좌,우로 한칸 대각선으로 두칸 씩 이동할 수 있다.")
     void move() {
-        //given
-        PiecePosition piecePosition = new PiecePosition(4, 3);
 
-        //when
-        SangMovePosition sangMovePosition = new SangMovePosition();
-
-        Movements moveAblePosition = sangMovePosition.calculateBasicMoveAbleDirections(piecePosition);
-
-        //then
-        assertThat(moveAblePosition.getValues()).hasSize(8)
-                .contains(
-                        Movement.create(Direction.UP, Direction.UP_LEFT, Direction.UP_LEFT),
-                        Movement.create(Direction.UP, Direction.UP_RIGHT, Direction.UP_RIGHT),
-                        Movement.create(Direction.DOWN, Direction.DOWN_LEFT, Direction.DOWN_LEFT),
-                        Movement.create(Direction.DOWN, Direction.DOWN_RIGHT, Direction.DOWN_RIGHT),
-                        Movement.create(Direction.LEFT, Direction.UP_LEFT, Direction.UP_LEFT),
-                        Movement.create(Direction.LEFT, Direction.DOWN_LEFT, Direction.DOWN_LEFT),
-                        Movement.create(Direction.RIGHT, Direction.UP_RIGHT, Direction.UP_RIGHT),
-                        Movement.create(Direction.RIGHT, Direction.DOWN_RIGHT, Direction.DOWN_RIGHT)
-                );
     }
 }
